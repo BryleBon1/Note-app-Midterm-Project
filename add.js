d77cbd43c71e66582616f767833498ffbc664d89
Bryle Bon <bonbrylenicole@gmail.com>
2022-10-18 16:54:32 +0800

    updated

---
+++ add.js
@@ -0,0 +1,14 @@
+const fs = require ('fs')
+
+const add = function (note, oldNote){
+    //parse oldNote from String to Object of arrays
+    const newNote = JSON.parse(oldNote)
+    //add new note to oldNote
+    newNote.push (note)
+
+    //save new note to note.txt again
+    fs.writeFileSync('note.txt', JSON.stringify(newNote))
+
+}
+
+module.exports = add
